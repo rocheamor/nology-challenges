dab638ff00a81a045669ae641a9344da24c69b8b
Roche Amor <rocheamor.gador@gmail.com>
2023-06-05 11:16:14 +1000

    adds folder called javaScript with html and js files for data types and variables

---
+++ javaScript/data-types.js
@@ -0,0 +1,13 @@
+console.log(typeof 15); // Prediction: {number}
+console.log(typeof 5.5); // Prediction: {number}
+console.log(typeof NaN); // Prediction: {number}
+console.log(typeof 'hello'); // Prediction: {string}
+console.log(typeof true); // Prediction: {boolean}
+console.log(typeof (1 != 2)); // Prediction: {boolean}
+console.log(typeof ('hamburger' + 's')); // Prediction: {string}
+console.log(typeof ('hamburgers' - 's')); // Prediction: {number}
+console.log(typeof ('1' + '3')); // Prediction: {string}
+console.log(typeof ('1' - '3')); // Prediction: {number}
+console.log(typeof ('johnny' + 5)); // Prediction: {string}
+console.log(typeof ('johnny' - 5)); // Prediction: {number}
+console.log(typeof (99 * 'luftbaloons')); // Prediction: {number}
---
+++ javaScript/variables.js
@@ -0,0 +1,11 @@
+/* Challenge: Storing Personal Information
+MVP:
+Create three variables to store your first name, last name and age.
+Assign values to these variables.
+Print a string containing all three variables in the console (should look like "Hi, my name is XXX XXX and I am XX years old) */
+
+let firstName = "Roche";
+let lastName = "Gador";
+let age = 33;
+
+console.log(`Hi, my name is ${firstName} ${lastName} and I am ${age} years old.`)
